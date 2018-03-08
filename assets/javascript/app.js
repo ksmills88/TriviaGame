2b36bbeefbe1209007621b0548ed144e2600a52a
kmills <ksmills82@gmail.com>
2018-03-08 02:55:57 -0500

    started JS with object array of questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+    var questions = [
+        { 
+            question: "Which of these is not a type of clef?",
+            choices: ["Treble", "Alto", "Canon", "Tenor"],
+            answer: "Canon",
+            pic: "assets/images/clefs.png"
+        },
+
+     {
+            question: "Which of the following is an instrument in the Woodwind family?",
+            choices: ["Saxophone", "Trumpet", "Baritone", "Xylophone"],
+            answer: "Saxophone",
+            pic: "assets/images/woodwind"
+        },
+
+     {
+            question: "If the section of music you are playing says 'tacet' what would you do?",
+            choices: ["play", "do not play", "transcribe", "clap the music"],
+            answer: "do not play",
+            pic: "assets/images/tacet.png"
+        },
+
+         {
+            question: "If your music says to play adagio, should you ___?",
+            choices: ["play agressively", "play softly", "play slowly", "play playfully"],
+            answer: "play slowly",
+            pic: "assets/images/slowjam.jpg"
+        },
+    
+         {
+            question: "If you are playing a C Major scale, which note ends the scale?",
+            choices: ["B", "C#", "C", "D"],
+            answer: "C",
+            pic: "assets/images/cmajor.png"
+        },
+
+         {
+            question: "How many different notes are there in a chromatic scale?",
+            choices: ["8", "10", "12", "13"],
+            answer: "12",
+            pic: "assets/images/chromatic.png"
+        },
+
+        {
+           question: "Which instrument is Kenny G most known for playing?",
+           choices: ["Bari Saxophone", "Soprano Saxophone", "Bassoon", "Clarinet"],
+           answer: "Soprano Saxophone",
+           pic: "assets/images/kennyg.jpg"
+        },
+
+        {
+           question: "Which American composer wrote the score to the Star Wars series?",
+           choices: ["John Williams", "Leonard Bernstein", "Eric Whitacre", "Danny Elfman"],
+           answer: "John Williams",
+           pic: "assets/images/johnwilliams.jpg"
+        }]
